5caf43e63b4b9e5c431864bfc0d6107208cf9f79
Deven Jahnke <10586846+DevenDoes@users.noreply.github.com>
2020-09-19 00:24:14 -0400

    Added webpack brower-sync
    Use "docker-compose run --rm --service-ports npm run watch" to expose ports required for browser-sync.

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
 
 /*
  |--------------------------------------------------------------------------
@@ -19,4 +21,26 @@ mix.js('resources/js/app.js', 'public/js')
             require('postcss-import'),
             require('tailwindcss'),
         ]
+    })
+    .webpackConfig({
+        plugins: [
+            new BrowserSyncPlugin(
+              // BrowserSync options
+              {
+                // browse to http://localhost:3000/ during development
+                host: 'localhost',
+                port: 3000,
+                // proxy the Webpack Dev Server endpoint
+                // (which should be serving on http://localhost:3100/)
+                // through BrowserSync
+                proxy: 'nginx:80'
+              },
+              // plugin options
+              {
+                // prevent BrowserSync from reloading the page
+                // and let Webpack Dev Server take care of this
+                reload: false
+              }
+            )
+          ],
     });
